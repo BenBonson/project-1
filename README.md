e6af978b23bf1b40a34231edec2ad7201d67d014
BenBonson <bonsonben@gmail.com>
2019-10-27 21:11:04 -0500

    Getting source code from files but http is linking correctly

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # project-1
+
+
+
+
+# records 
+1. started with navigation, tried multiple html pages but figured that this should be more go centered
+2. restarted and made an index html
+3. index displays source code when in its own file but does it properly in the same one
+4. server stopped working
+5. in confusion tested older projects and examples, they don't work either...eventhough I haven't touched them...very confused
+6. localhost:9000 wont work tried 3000 and 7000 and they work ???
+7. Now index displays its source code in the same file ???
+8. Lots of testing and things only broke more now attempting to connect to local host says connection was reset
+9. found and fixed the issue but it still displays the source code not in html format
--- main.go
+++ main.go
@@ -1,98 +1,113 @@
 package main
 
 import (
-	"bufio"
-	"expvar"
-	"fmt"
 	"html/template"
-	"io"
 	"net/http"
 	"os/exec"
 )
 
-var myCount = expvar.NewInt("my.count")
-var myStatus = expvar.NewString("my.status")
+//var top = exec.Command("top")
 
 func main() {
 	// Sets up a file server in current directory
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	//http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/top", topfunc)
 
 	//For each endpoint, call a function described below
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/hellohtml", hellohtml)
-	http.HandleFunc("/formsubmit", formsubmit)
-	http.HandleFunc("/template", hellotemphandler)
-	http.ListenAndServe(":9000", nil)
+	// http.HandleFunc("/hello", hello)
+	// //http.HandleFunc("/logs", logs)
+	// http.HandleFunc("/hellohtml", hellohtml)
+	// http.HandleFunc("/formsubmit", formsubmit)
+	// http.HandleFunc("/template", hellotemphandler)
+	http.ListenAndServe(":7000", nil)
 	//pretty sure l and s is what sets default index page to be localhost:9000
-	ssh()
+	// ssh()
 }
 
-func ssh() {
-	cmd := exec.Command("ssh", "user1@192.168.56.102")
-	reader, _ := cmd.StdoutPipe()
-	scanner := bufio.NewScanner(reader)
-	go func() {
-		for scanner.Scan() {
-			fmt.Println("user1> ", scanner.Text())
-		}
-	}()
-	cmd.Start()
-	cmd.Wait()
+func index(response http.ResponseWriter, request *http.Request) {
+	temp, _ := template.ParseFiles("html/index.html")
+	//temp.Execute(response, Signin)
+	temp.Execute(response, nil)
+	//temp.Execute(response, temp)
 }
 
-func hello(response http.ResponseWriter, request *http.Request) {
-	name := request.FormValue("name")
-	output := []byte("Hello " + name)
-	fmt.Println(name, "says hello!")
-	response.Write(output)
+func topfunc(response http.ResponseWriter, request *http.Request) {
+	temp, _ := template.ParseFiles("html/top.html")
+	//cmd := exec.Command("top")
+	//temp.Execute(response, Signin)
+	top := exec.Command("top")
+	temp.Execute(response, top)
+	//temp.Execute(response, nil)
 }
 
-//prints what is in html on this page
-func hellohtml(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-Type", "text/html")
-	//output := []byte("<html><body><h1>Hello There!</h1></body></html>")
-	//response.Write(output)
-	io.WriteString(response, `
-	<DOCTYPE html>
-	<html>
-	<head>
-		<title>My Page</title>
-	</head>
-	<body>
-		<h2>Welcome to My Page</h2>
-		<p>This is a test of a go server</p>
-	</body>
-	</html>
-	`)
-}
+// func ssh() {
+// 	cmd := exec.Command("ssh", "user1@192.168.56.102")
+// 	reader, _ := cmd.StdoutPipe()
+// 	scanner := bufio.NewScanner(reader)
+// 	go func() {
+// 		for scanner.Scan() {
+// 			fmt.Println("user1> ", scanner.Text())
+// 		}
+// 	}()
+// 	cmd.Start()
+// 	cmd.Wait()
+// }
 
-//prints nothing by itself
-//however if localhost:9000 is run first and the form filled it will print welcome username and their password
-//in command line
-func formsubmit(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("Welcome", request.FormValue("user"))
-	fmt.Println("Your password is", request.FormValue("password"))
-}
+// func hello(response http.ResponseWriter, request *http.Request) {
+// 	navigation
+// 	name := request.FormValue("name")
+// 	output := []byte("Hello " + name)
+// 	fmt.Println(name, "says hello!")
+// 	response.Write(output)
+// }
 
-//template stuff prints Hello, Bob!
-const hellotemplate = `
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Template Page</title>
-	</head>
-	<body>
-		<h1>Hello, {{.Name}}!</h1>
-	</body>
-</html>
-`
+// //prints what is in html on this page
+// func hellohtml(response http.ResponseWriter, request *http.Request) {
+// 	response.Header().Set("Content-Type", "text/html")
+// 	//output := []byte("<html><body><h1>Hello There!</h1></body></html>")
+// 	//response.Write(output)
+// 	io.WriteString(response, `
+// 	<DOCTYPE html>
+// 	<html>
+// 	<head>
+// 		<title>My Page</title>
+// 	</head>
+// 	<body>
+// 		<h2>Welcome to My Page</h2>
+// 		<p>This is a test of a go server</p>
+// 	</body>
+// 	</html>
+// 	`)
+// }
 
-var hellotmpl = template.Must(template.New(".").Parse(hellotemplate))
+// //prints nothing by itself
+// //however if localhost:9000 is run first and the form filled it will print welcome username and their password
+// //in command line
+// func formsubmit(response http.ResponseWriter, request *http.Request) {
+// 	fmt.Println("Welcome", request.FormValue("user"))
+// 	fmt.Println("Your password is", request.FormValue("password"))
+// }
 
-func hellotemphandler(response http.ResponseWriter, request *http.Request) {
-	myCount.Add(1)
-	myStatus.Set("Good")
-	hellotmpl.Execute(response, map[string]interface{}{
-		"Name": "Bob",
-	})
-}
+// //template stuff prints Hello, Bob!
+// const hellotemplate = `
+// <!DOCTYPE html>
+// <html>
+// 	<head>
+// 		<title>Template Page</title>
+// 	</head>
+// 	<body>
+// 		<h1>Hello, {{.Name}}!</h1>
+// 	</body>
+// </html>
+// `
+
+// var hellotmpl = template.Must(template.New(".").Parse(hellotemplate))
+
+// func hellotemphandler(response http.ResponseWriter, request *http.Request) {
+// 	myCount.Add(1)
+// 	myStatus.Set("Good")
+// 	hellotmpl.Execute(response, map[string]interface{}{
+// 		"Name": "Bob",
+// 	})
+// }
