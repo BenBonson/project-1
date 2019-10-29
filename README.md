4100e6f505cf5247ba8ce839610e31a6bd762bfa
BenBonson <bonsonben@gmail.com>
2019-10-28 19:58:23 -0500

    Ugly but works

--- README.md
+++ README.md
@@ -14,3 +14,13 @@
 8. Lots of testing and things only broke more now attempting to connect to local host says connection was reset
 9. found and fixed the issue but it still displays the source code not in html format
 10. added response.Header().Set("Content-Type", "text/html; charset=utf-8") now html displays correctly
+11. Tried to work on getting top to work, its not displayig anything
+12. main.go now displays all the html with top included, however it does so in the command line
+13. Got the results to print in HTML, its pretty ugly
+
+
+#notes/references
+
+https://www.calhoun.io/intro-to-templates-p1-contextual-encoding/
+
+https://stackoverflow.com/questions/1877045/how-do-you-get-the-output-of-a-system-command-in-go
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
 )
 
 //var top = exec.Command("top")
+
+// type Topcom struct {
+// 	TOP []string
+// }
 
 func main() {
 	// Sets up a file server in current directory
@@ -35,23 +42,162 @@ func index(response http.ResponseWriter, request *http.Request) {
 	//temp.Execute(response, temp)
 }
 
+//prints out all html with top included in commandline????
+// func topfunc(response http.ResponseWriter, request *http.Request) {
+// 	temp, _ := template.ParseFiles("html/top.html")
+// 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	//cmd := exec.Command("top")
+// 	//temp.Execute(response, Signin)
+// 	//top := exec.Command("top")
+
+// 	top, err := exec.Command("top", "-b", "-n", "1").Output()
+// 	if err != nil {
+// 		fmt.Println("here")
+// 		log.Fatal(err)
+// 	}
+// 	tops := string(top)
+// 	//fmt.Println(tops)
+
+// 	data := struct{ TOP string }{tops}
+
+// 	err = temp.Execute(os.Stdout, data)
+// 	if err != nil {
+// 		fmt.Println("error two")
+// 		panic(err)
+// 	}
+// 	temp.Execute(response, nil)
+// }
+
+//top gets the results of the top command and sends the results to the html page its ugly as sin but it works
 func topfunc(response http.ResponseWriter, request *http.Request) {
 	temp, _ := template.ParseFiles("html/top.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	//cmd := exec.Command("top")
 	//temp.Execute(response, Signin)
-	top := exec.Command("top")
-	temp.Execute(response, top)
-	//temp.Execute(response, nil)
+	//top := exec.Command("top")
+
+	top, err := exec.Command("top", "-b", "-n", "1").Output()
+	if err != nil {
+		fmt.Println("here")
+		log.Fatal(err)
+	}
+	tops := string(top)
+	//fmt.Println(tops)
+
+	data := struct{ TOP string }{tops}
+
+	err = temp.Execute(os.Stdout, data)
+	if err != nil {
+		fmt.Println("error two")
+		panic(err)
+	}
+	temp.Execute(response, data)
 }
 
+// func topfunc(w http.ResponseWriter, r *http.Request) {
+// 	temp, _ := template.ParseFiles("top.html")
+// 	cmd := exec.Command("top", "-b", "-n", "1")
+// 	stdout, err := cmd.Output()
+
+// 	var count int = 0
+// 	if err != nil {
+// 		println(err.Error())
+// 		return
+// 	}
+
+// 	t := Topcom{TOP: make([]string, 1)}
+
+// 	for i := 1; i < len(stdout); i++ {
+// 		if stdout[i] != 10 {
+// 			t.TOP[count] += string(stdout[i])
+// 		} else {
+// 			count++
+// 			t.TOP = append(t.TOP, "")
+// 		}
+// 	}
+// 	// output := string(stdout)
+// 	// cmd.Start()
+// 	// cmd.Wait()
+// 	// fmt.Println(cmd)
+// 	temp.Execute(w, t)
+// }
+
+//combined w/gmac
+// func topfunc(w http.ResponseWriter, r *http.Request) {
+// 	temp, _ := template.ParseFiles("applications.html")
+// 	cmd := exec.Command("top", "-b", "-n", "1")
+// 	cmdout, stderr := cmd.Output()
+// 	t := Topcom{TOP: make([]string, 1)}
+// 	var count int = 0
+
+// 	if stderr != nil {
+// 		fmt.Println(stderr)
+// 	}
+// 	for i := 1; i < len(cmdout); i++ {
+// 		if cmdout[i] != 10 {
+// 			t.TOP[count] += string(cmdout[i])
+// 		} else {
+// 			count++
+// 			t.TOP = append(t.TOP, "")
+// 		}
+// 	}
+
+// 	temp.Execute(w, t)
+// }
+
+//prints out in command line but not html
+// func topfunc(response http.ResponseWriter, request *http.Request) {
+// 	temp, _ := template.ParseFiles("html/top.html")
+// 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	//cmd := exec.Command("top")
+// 	//temp.Execute(response, Signin)
+// 	//top := exec.Command("top")
+
+// 	cmd := exec.Command("top", "-b", "-n", "1")
+// 	stdout, err := cmd.Output()
+
+// 	if err != nil {
+// 		println(err.Error())
+// 		return
+// 	}
+
+// 	tops := string(stdout)
+
+// 	data := struct{ TOP string }{tops}
+
+// 	err = temp.Execute(os.Stdout, data)
+// 	if err != nil {
+// 		fmt.Println("error two")
+// 		panic(err)
+// 	}
+// 	print(string(stdout))
+
+// 	//temp.Execute(response, top)
+// 	//temp.Execute(response, nil)
+// }
+
+//logfunc gets the logs and sends the results to the html page its ugly as sin but it works
 func logfunc(response http.ResponseWriter, request *http.Request) {
 	temp, _ := template.ParseFiles("html/log.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	//cmd := exec.Command("top")
 	//temp.Execute(response, Signin)
-	log := exec.Command("tail", "-f", "-n", "5", "/var/log/syslog")
-	temp.Execute(response, log)
+	log, err := exec.Command("tail", "-n", "5", "/var/log/syslog").Output()
+	if err != nil {
+		fmt.Println("here")
+		panic(err)
+	}
+	logs := string(log)
+	//fmt.Println(tops)
+
+	data := struct{ LOG string }{logs}
+
+	err = temp.Execute(os.Stdout, data)
+	if err != nil {
+		fmt.Println("error two")
+		panic(err)
+	}
+	temp.Execute(response, data)
 	//temp.Execute(response, nil)
 }
 
